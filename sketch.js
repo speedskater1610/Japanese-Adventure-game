b2857b67e75376ea2a4b1e7574d5c9f8421082c0
speedskater1610 <speedskater1610@gmail.com>
2025-07-18 01:31:30 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
-/// @breif first thing to run 
+/// @breif first thing to run (init)
 function setup() {
   // set up the graphics window to be 75% of the users window 
   createCanvas(window.innerWidth * 0.75, window.innerHeight * 0.75);
 }
 
-/// @breif draw things on the screen from here
+/// @breif draw things on the screen from here (main loop at 60fps)
 function draw() {
   background(220);
 }
