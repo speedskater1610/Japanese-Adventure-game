da07d253ac7efb8e04bd5db3ca939441564bf1ce
speedskater1610 <speedskater1610@gmail.com>
2025-07-18 01:40:04 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
+  //create the player and set them up
+  let player = new Player("defualt", 1);
+  console.log(player);
+
 /// @breif first thing to run (init)
 function setup() {
   // set up the graphics window to be 75% of the users window 
   createCanvas(window.innerWidth * 0.75, window.innerHeight * 0.75);
-  
-  //create the player and set them up
-  let player = new Player("defualt", 1);
-  console.log(player);
+
 }
 
 /// @breif draw things on the screen from here (main loop at 60fps)
